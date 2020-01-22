76aa1ac8464c0a423f54eb984016c1858a83104f
chandrab <chandrab@allenai.org>
2020-01-22 11:48:29 -0800

    Pass args

--- anli/demo.py
+++ anli/demo.py
@@ -57,7 +57,7 @@ def _predict(data_processor, tokenizer, model, obs1, obs2, hyp1, hyp2, device):
     return answer
 
 
-def main(args, model_path, interactive=False):
+def main(args, interactive=False):
     device = torch.device(args.gpu_id if torch.cuda.is_available() else "cpu")
 
     data_processor, tokenizer, model = load_anli_model(args.model_name_or_path, device)
