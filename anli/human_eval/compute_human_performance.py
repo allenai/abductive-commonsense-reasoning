739127b98f78d2309a34533d11f3873be9b035fc
chandrab <chandrab@allenai.org>
2020-02-25 14:22:57 -0800

    Addressed review comments

--- anli/human_eval/compute_human_performance.py
+++ anli/human_eval/compute_human_performance.py
@@ -19,37 +19,40 @@ def main(args):
     per_story_votes = {}
     per_story_workers = {}
     per_story_per_vote_worktime = {}
+
+    story_id_field = 'Input.story_id'
+
     for r in records:
-        if r['Input.story_id'] not in per_story_votes:
-            per_story_votes[r['Input.story_id']] = []
-            per_story_workers[r['Input.story_id']] = []
-            per_story_per_vote_worktime[r['Input.story_id']] = []
-        per_story_votes[r['Input.story_id']].append(r['Answer.Answer_radios'])
-        per_story_workers[r['Input.story_id']].append(_hash(r['WorkerId']))
-        per_story_per_vote_worktime[r['Input.story_id']].append(r['WorkTimeInSeconds'])
+        if r[story_id_field] not in per_story_votes:
+            per_story_votes[r[story_id_field]] = []
+            per_story_workers[r[story_id_field]] = []
+            per_story_per_vote_worktime[r[story_id_field]] = []
+        per_story_votes[r[story_id_field]].append(r['Answer.Answer_radios'])
+        per_story_workers[r[story_id_field]].append(_hash(r['WorkerId']))
+        per_story_per_vote_worktime[r[story_id_field]].append(r['WorkTimeInSeconds'])
 
     stories = []
     correct = 0
     done = set()
     for r in records:
-        if r['Input.story_id'] in done:
+        if r[story_id_field] in done:
             continue
-        done.add(r['Input.story_id'])
+        done.add(r[story_id_field])
 
-        assert len(per_story_votes[r['Input.story_id']]) == 3
+        assert len(per_story_votes[r[story_id_field]]) == 3
 
-        majority_vote = Counter(per_story_votes[r['Input.story_id']]).most_common(1)[0][0]
+        majority_vote = Counter(per_story_votes[r[story_id_field]]).most_common(1)[0][0]
         stories.append({
-            'story_id': r['Input.story_id'],
+            'story_id': r[story_id_field],
             'obs1': r['Input.obs1'],
             'obs2': r['Input.obs2'],
             'hyp1': r['Input.hyp1'],
             'hyp2': r['Input.hyp2'],
             'label': r['Input.label'],
-            'votes': per_story_votes[r['Input.story_id']],
+            'votes': per_story_votes[r[story_id_field]],
             'majority_vote': majority_vote,
-            'workers': per_story_workers[r['Input.story_id']],
-            'worktime': per_story_per_vote_worktime[r['Input.story_id']]
+            'workers': per_story_workers[r[story_id_field]],
+            'worktime': per_story_per_vote_worktime[r[story_id_field]]
         })
 
         if majority_vote == r['Input.label']:
@@ -66,8 +69,8 @@ if __name__ == '__main__':
         description='Script to compute corpus satistics')
 
     # Required Parameters
-    parser.add_argument('--input_file', type=str, help='Location of dev data', default=None)
-    parser.add_argument('--output_file', type=str, help='Location of dev data', default=None)
+    parser.add_argument('--input_file', type=str, help='Location of human evaluation data from MTurk.', default=None)
+    parser.add_argument('--output_file', type=str, help='Location of file to save results.', default=None)
 
     args = parser.parse_args()
     print('====Input Arguments====')
